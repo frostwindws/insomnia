968e9a1f5a9f86247f6765767fd844b0fd70439f
Frostwind <friospirit@mail.ru>
2021-12-13 22:36:50 +0200

    initial commit

---
+++ Insomnia/InsomniaService.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Insomnia;
+
+/// <summary>
+/// Prevent sleeping mode service.
+/// </summary>
+public class InsomniaService : IDisposable
+{
+    [Flags]
+    private enum ExecutionState : uint
+    {
+        SystemRequired = 0x00000001,
+        DisplayRequired = 0x00000002,
+        AwayModeRequired = 0x00000040,
+        Continuous = 0x80000000
+    }
+
+    private readonly ExecutionState _originalState;
+
+    private InsomniaService()
+    {
+        _originalState = SetThreadExecutionState(ExecutionState.SystemRequired |
+                                                 ExecutionState.DisplayRequired |
+                                                 ExecutionState.AwayModeRequired |
+                                                 ExecutionState.Continuous);
+    }
+
+    /// <inheritdoc cref="IDisposable"/>.
+    public void Dispose() => SetThreadExecutionState(_originalState);
+
+    /// <summary>
+    /// Prevent sleeping mode.
+    /// </summary>
+    /// <returns>True if operation was successful.</returns>
+    public static InsomniaService PreventSleep() => new();
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern ExecutionState SetThreadExecutionState(ExecutionState flags);
+}
---
+++ Insomnia/Program.cs
@@ -0,0 +1,7 @@
+﻿using Insomnia;
+
+using (InsomniaService.PreventSleep())
+{
+    Console.WriteLine("Press any key to stop insomnia...");
+    Console.ReadKey();
+}
